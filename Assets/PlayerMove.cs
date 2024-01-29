897ddfdfc280d5e6b4efa2970836a779a0cff79c
Jack Tomlinson <jackotomlinson1@googlemail.com>
2024-01-29 23:40:45 +0000

    FPS Camera, Basic movement, Basic environment

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Vector3 _moveDirection;
+    public Transform Orientation;
+    private float moveSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        _moveDirection = Orientation.forward * vertical + Orientation.right * horizontal;
+
+        transform.position += _moveDirection.normalized * moveSpeed * Time.deltaTime;
+    }
+}
