e9ce7f0be7dd4983f542c60056894c077f9bb87d
TheReckoning <jdw_liverpool@hotmail.co.uk>
2024-02-04 19:19:47 +0000

    Moving shit around

--- Assets/PlayerInventory/PlayerStats.cs
+++ Assets/PlayerInventory/PlayerStats.cs
@@ -1,55 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using System.Xml.XPath;
 
 public class PlayerStats : MonoBehaviour
 {
-
-    public double currentXp=0;
-    public double currentLevel=0;
-    public double requiredXp=100;
+    public double currentXp = 0;
+    public double currentLevel = 0;
+    public double requiredXp = 100;
     public PlayerHud playerHud;
     public bool passed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialization code (if needed)
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(passed)
+        if (passed)
         {
-            Debug.Log("passed");
+            Debug.Log("Passed");
         }
-        
-
     }
 
-    public void AddXp(double i)
+    public void AddXp(double xpAmount)
     {
-        currentXp+=i;
+        currentXp += xpAmount;
         AddLevel();
-        Debug.Log($"xp added {currentXp}");
+        Debug.Log($"XP added: {currentXp}");
     }
+
     public void AddLevel()
     {
-        if(currentXp>requiredXp)
+        if (currentXp > requiredXp)
         {
-            currentLevel+=1;
-            requiredXp = (requiredXp + 100)*1.1;
-        }
-        else
-        {
-
+            currentLevel++;
+            requiredXp = (requiredXp + 100) * 1.1;
         }
 
-        playerHud.updateXpText(currentXp,currentLevel,requiredXp);
+        playerHud.updateXpText(currentXp, currentLevel, requiredXp);
     }
 }
-
-
